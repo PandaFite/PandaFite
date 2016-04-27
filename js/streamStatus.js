4cd545ee8a864ff84b23197f5ca27d0bf2f0ce0e
matt <matt@mattunderscore.co>
2016-04-27 16:14:10 -0700

    minor style edits

--- js/streamStatus.js
+++ js/streamStatus.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
       console.log('Stream Data:', streamData)
       if(streamData && streamData.stream) {
          $.getJSON('https://api.twitch.tv/kraken/channels/' + twitchName + '?callback=?', function(data) {
-			document.getElementById('title').textContent = data.status;
+			document.getElementById('title').textContent = "Stream is live - \"" + data.status + "\"";
 			document.getElementById('player').src = "https://player.twitch.tv/?channel=" + twitchName;
 });
       } else {
-         document.getElementById('title').textContent = "Most Recent YouTube Video";
+         document.getElementById('title').textContent = "Stream is offline - Most Recent YouTube Video:";
 		 document.getElementById('player').src = "https://www.youtube.com/embed?listType=user_uploads&list=" + youtubeName;
       }
    })
