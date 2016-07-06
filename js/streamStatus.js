8c07a8c831d958c879ab80bd820bbac3fd753a73
matt <matt@mattunderscore.co>
2016-07-06 14:20:01 -0700

    Update streamStatus to fetch title with Client-ID

--- js/streamStatus.js
+++ js/streamStatus.js
@@ -1,17 +1,11 @@
-$(document).ready(function() {   
-
-	var twitchName = "pandaplayshd";
-	var youtubeName = "pandaplaysmchd";
-   $.getJSON("https://api.twitch.tv/kraken/streams/" + twitchName + "?callback=?",function(streamData) {
-      console.log('Stream Data:', streamData)
-      if(streamData && streamData.stream) {
-         $.getJSON('https://api.twitch.tv/kraken/channels/' + twitchName + '?callback=?', function(data) {
-			document.getElementById('title').textContent = "Stream is live - \"" + data.status + "\"";
-			document.getElementById('player').src = "https://player.twitch.tv/?channel=" + twitchName;
+$.ajax({
+ type: 'GET',
+ url: 'https://api.twitch.tv/kraken/channels/pandaplayshd',
+ headers: {
+   'Client-ID': 'axjhfp777tflhy0yjb5sftsil'
+ },
+ success: function(data) {
+   console.log(data);
+   document.getElementById('title').textContent = data.status;
+ }
 });
-      } else {
-         document.getElementById('title').textContent = "Stream is offline";
-		 document.getElementById('player').src = "https://pandaplayshd.com/offline";
-      }
-   })
-})
