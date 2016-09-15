5f8ec9b5ce662070c4855f41b618594c30d94542
matt <matt@mattunderscore.co>
2016-09-15 15:55:13 -0700

    error 400 hotfix

--- js/status.js
+++ js/status.js
@@ -2,7 +2,7 @@ $.ajax({
  type: 'GET',
  url: 'https://api.twitch.tv/kraken/streams/' + username,
  headers: {
-   'Client-ID': 'axjhfp777tflhy0yjb5sftsil'
+   'Client-ID': 'f2cmg4s30fnzmq7zbcx8rcsfxdc1san'
  },
  success: function(data) {
    console.log(data);
