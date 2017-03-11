79ec73e37e74b677d00c3e33223d83a4162258f7
matt3541 <matt@mattunderscore.co>
2017-03-11 01:19:16 -0800

    commit new site

--- js/status.js
+++ js/status.js
@@ -21,7 +21,7 @@ $.ajax({
    {
 	$.ajax({
 	 type: 'GET',
-	 url: 'https://cors-anywhere.herokuapp.com/https://tmi.twitch.tv/hosts?include_logins=1&host=53010272',
+	 url: 'https://cors-anywhere.herokuapp.com/https://tmi.twitch.tv/hosts?include_logins=1&host=' + userId,
 	 success: function(data) {
 	   console.log(data);
 	   if (data.hosts[0].target_login == null)
@@ -29,14 +29,8 @@ $.ajax({
 			streamOffline();
 	   }
 	   else
-	   {
-		//define pressChat function for this situation
-		pressChat = function() {
-			document.getElementById('chatcover').innerHTML = "<iframe id='chatframe' style='border: none; height: 430px; width: 585px;' src='https://www.twitch.tv/" + data.hosts[0].target_login + "/chat'></iframe>"
-		}	   
+	   {   
 		 
-		//set html for chat cover
-		document.getElementById('chatcover').innerHTML = "<a href='#' onclick='pressChat(); return false;' style='display: block; width: 585px; height: 430px;'><span style='display: block;height: 430px; width: 585px; display: block; text-align: center;'><span style='font-size: 26px; line-height: 430px;'>Click to view chat</span></span></a>";
 		
 		//set thumbnail to that of the hosted streamer
 		document.getElementById('vod-thumbnail').src = "https://static-cdn.jtvnw.net/previews-ttv/live_user_" + data.hosts[0].target_login + "-1170x659.jpg";
@@ -64,15 +58,9 @@ $.ajax({
 		   console.log(data);
 		   if (data.stream) {
 		   
-		   document.getElementById('title').textContent = "Panda is currently hosting " + data.stream.channel.display_name + ": " + data.stream.channel.status;
-		   if (data.stream.game == "Creative")
-		   {
-			   document.getElementById('streaminfo').textContent = " | Being " + data.stream.game + " for " + data.stream.viewers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " viewers and " + data.stream.channel.followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " followers";
-		   }
-		   else{
-		   
-		   document.getElementById('streaminfo').textContent = " | Playing " + data.stream.game + " for " + data.stream.viewers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " viewers and " + data.stream.channel.followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " followers";
-		   }
+		   document.getElementById('title').textContent = "Panda is currently hosting " + data.stream.channel.display_name;  
+		   document.getElementById('streaminfo').textContent = data.stream.channel.status;
+		   chatSrc = "https://www.twitch.tv/" + data.stream.channel.name + "/chat";
 		   timer();
 		   }
 		   else {streamOffline();}
--- js/streamFunctions.js
+++ js/streamFunctions.js
@@ -4,57 +4,27 @@
  * Proprietary and confidential
  */
 var username = "pandaplayshd";
-
+var userId = "53010272";
+var chatSrc;
 var pressPlay;
-
-var pressChat;
-
-var uptime;
 
 function displayTitle()
 {
 	function getInfo(){
 			
-$.ajax({
- type: 'GET',
- url: 'https://api.twitch.tv/kraken/streams/' + username,
- headers: {
-   'Client-ID': 'f2cmg4s30fnzmq7zbcx8rcsfxdc1san'
- },
- success: function(data) {
-   console.log(data);
-   document.getElementById('title').textContent = "\u2022 LIVE: " + data.stream.channel.status;
-   if (data.stream.game == "Creative")
-   {
-	   document.getElementById('streaminfo').textContent = " | Being " + data.stream.game + " for " + data.stream.viewers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " viewers and " + data.stream.channel.followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " followers";
-   }
-   else{
-   
-   document.getElementById('streaminfo').textContent = " | Playing " + data.stream.game + " for " + data.stream.viewers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " viewers and " + data.stream.channel.followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " followers";
-   }
- }
-});	
 		$.ajax({
-		url: 'https://api.rtainc.co/twitch/uptime?channel=' + username,
-		success: function (data) {
-			var name = data.split(' ');
-			var noComma = name[1].split(',');
-			if (name[1] == "is" && name[2] == "not")
-			{
-				document.getElementById('uptime').textContent = "(uptime api error)";
-			}
-			else if (name[3] == "seconds")
-			{
-				document.getElementById('uptime').textContent = "(" + name[0] + " " + noComma[0] + " uptime)";
-			}
-			else
-			{
-			document.getElementById('uptime').textContent = "(" + data + " uptime)";
-			}
-		},
-		dataType: "html"
-		});
-	}
+		 type: 'GET',
+		 url: 'https://api.twitch.tv/kraken/streams/' + username,
+		 headers: {
+		   'Client-ID': 'f2cmg4s30fnzmq7zbcx8rcsfxdc1san'
+		 },
+		 success: function(data) {
+		   console.log(data);
+		   document.getElementById('title').textContent = "LIVE: " + data.stream.channel.status;	
+		   document.getElementById('streaminfo').textContent = "Playing " + data.stream.game + " for " + data.stream.viewers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " viewers and " + data.stream.channel.followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " followers";
+		 }
+		});	
+	} // end getInfo
 	
 	getInfo();
 	setInterval(getInfo,10000);
@@ -71,32 +41,7 @@ function onlineFrame()
 		document.getElementById('button-play-link').style.visibility = "hidden";
 		}, 2500);	
 		}
-		
-	//document.getElementById('chatcover').innerHTML = "<a href='#' onclick='pressChat(); return false;' style='display: block; width: 585px; height: 430px;'><span style='display: block;height: 430px; width: 585px; display: block; text-align: center;'><span style='font-size: 26px; line-height: 430px;'>Click to view chat</span></span></a>";
-	
-	pressChat = function() {
-	document.getElementById('chatcover').innerHTML = "<iframe id='chatframe' style='border: none; height: 430px; width: 585px;' src='https://www.twitch.tv/" + username + "/chat'></iframe>"
-	}
-
-	
-	timer();
-}
-
-var count = 8;
-var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
-var stopTimer;
-function timer() {
-    if (stopTimer != 1) {
-        count = count - 1;
-        if (count <= 0) {
-            clearInterval(counter);
-            pressChat();
-            stopTimer = 1;
-            return;
-        }
-
-        document.getElementById("chatcover").innerHTML = "<a href='#' onclick='return false;' style='display: block; width: 585px; height: 430px;'><span style='display: block;height: 430px; width: 585px; display: block; text-align: center;'><span style='font-size: 26px; line-height: 430px;'>Chat loading in " + count + "&hellip;</span></span></a>";
-    }
+	chatSrc = "https://www.twitch.tv/" + username + "/chat";
 }
 
 function ytDisplay() {
@@ -118,7 +63,6 @@ function ytDisplay() {
 
 function streamOffline()
 {
-    stopTimer = 1;
 	$.ajax({
 	 type: 'GET',
 	 url: 'https://api.twitch.tv/kraken/channels/' + username + '/videos?broadcasts=true',
@@ -162,54 +106,6 @@ function streamOffline()
 		var thumbHD = noRes + "-1170x659.jpg"
 	   document.getElementById('vod-thumbnail').src = thumbHD;
 	   }
-	   
-	   
-	   
-		//var str = data.videos[0].created_at;
-		//var res = str.split("-");
-		//var day = res[2].split("T");
-		
-
-		//var realDay = day[0] - 1;
-		//var fullDate = res[1] + "/" + realDay + "/" + res[0];
-
-		
-		//var timeFull = day[1].split("Z");
-		//var time = timeFull[0].split(":");
-		//var hour = time[0];
-		
-
-		//if (time[0] == 0)
-		//{
-		//	hour = 4; 
-		//}
-		//else if (time[0] >= 1 && time[0] <= 7)
-		//{
-		//	hour = time[0] + 4; 
-		//}
-		//else if (time[0] >= 8 && time[0] <= 19)
-		//{
-		//	hour = time[0] - 8 
-		//}
-		//else if (time[0] >= 20 && time [0] <= 24)
-		//{
-		//	hour = time[0] - 20; 
-		//}
-		
-		
-		
-		//var minute = time[1];
-		//var timeOfDay = "AM"
-		
-		//if (time[0] >= 8 && time[0] <= 19)
-		//{
-		//	timeOfDay = " AM PST";
-		//}	
-		//else
-		//{
-		//	timeOfDay = " PM PST";
-		//}	
-
 		document.getElementById('title').textContent = "Most recent broadcast:";
 		document.getElementById('streaminfo').textContent = " " + data.videos[0].title;
 		
@@ -229,8 +125,7 @@ function streamOffline()
 		
 	 }
 	});
-	document.getElementById('chatcover').innerHTML = "<iframe src='https://discordapp.com/widget?id=221059861457141770&theme=dark' style='border: none; height: 430px; width: 585px'></iframe>";
-	document.getElementById('chatcover').style.backgroundColor = "transparent";
+	chatSrc = "https://www.twitch.tv/" + username + "/chat";
 }
 
 function playerError() {
