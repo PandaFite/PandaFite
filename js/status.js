928e909bd59e84b4a0e2a6b780b1562d2531ab7b
Matt Jones <mattj456@gmail.com>
2017-12-21 22:43:36 -0800

    Begin conversion to new username, tweak some things and remove obsolete player functions

--- js/status.js
+++ js/status.js
@@ -14,38 +14,21 @@ $.ajax({
    console.log(data);
    if (data.stream)
    {
-   displayTitle();
-   onlineFrame();
+   online();
    }
-   else 
+   else
    {
 	$.ajax({
 	 type: 'GET',
 	 url: 'https://cors-anywhere.herokuapp.com/https://tmi.twitch.tv/hosts?include_logins=1&host=' + userId,
 	 success: function(data) {
 	   console.log(data);
-	   if (data.hosts[0].target_login == null)
-	   {
+	   if (data.hosts[0].target_login == null) {
 			streamOffline();
 	   }
-	   else
-	   {   
-		 
-		
-		//set thumbnail to that of the hosted streamer
-		document.getElementById('vod-thumbnail').src = "https://static-cdn.jtvnw.net/previews-ttv/live_user_" + data.hosts[0].target_login + "-1170x659.jpg";
-		
-		//define pressPlay function for this situation
-		pressPlay = function() {
-		document.getElementById('button-play-span').style.background = "url(img/loading-ring.svg) no-repeat center center";
-		document.getElementById('player').src = "https://player.twitch.tv/?channel=" + data.hosts[0].target_login +"&muted";
-		setTimeout(function() {
-		document.getElementById('vod-thumbnail').style.visibility = "hidden";
-		document.getElementById('button-play-link').style.visibility = "hidden";
-		}, 2500);
-		
-		}
-		
+	   else {
+       document.getElementById('player').src = "https://player.twitch.tv/?channel=" + data.hosts[0].target_login +"&autplay=false";
+
 		//get title, viewers, game, etc from hosted streamer
 		function getHostInfo() {
 		$.ajax({
@@ -57,12 +40,12 @@ $.ajax({
 		 success: function(data) {
 		   console.log(data);
 		   if (data.stream) {
-		   
-		   document.getElementById('title').textContent = "Panda is currently hosting " + data.stream.channel.display_name;  
+
+		   document.getElementById('title').textContent = "Panda is currently hosting " + data.stream.channel.display_name;
 		   document.getElementById('streaminfo').textContent = data.stream.channel.status;
 		   chatSrc = "https://www.twitch.tv/" + data.stream.channel.name + "/chat";
 		   }
-		   else {streamOffline();}
+		   else streamOffline();
 		 }
 		});
 
@@ -71,7 +54,7 @@ $.ajax({
 		//auto update 10s interval
 		getHostInfo();
 		setInterval(getHostInfo,10000);
-		
+
 	   }
 	 },
 	 error: function()
