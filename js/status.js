6f54403db19b686fafb9bf36e29b25390499eaaa
matt-t-jones <matt-t-jones@protonmail.com>
2018-01-21 00:34:51 -0800

    Code cleanup & fixes
    Replace native JS selectors with jquery
    
    Fix #18

--- js/status.js
+++ js/status.js
@@ -1,70 +1,67 @@
-/* status.js
- * Copyright (C) mattunderscore.us - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- */
+/*
+    status.js
+    Copyright (C) Matt Jones - All Rights Reserved
+*/
 ytDisplay();
 $.ajax({
- type: 'GET',
- url: 'https://api.twitch.tv/kraken/streams/' + username,
- headers: {
-   'Client-ID': 'f2cmg4s30fnzmq7zbcx8rcsfxdc1san'
- },
- success: function(data) {
-   console.log(data);
-   if (data.stream)
-   {
-   online();
-   }
-   else
-   {
-	$.ajax({
-	 type: 'GET',
-	 url: 'https://cors-anywhere.herokuapp.com/https://tmi.twitch.tv/hosts?include_logins=1&host=' + userId,
-	 success: function(data) {
-	   console.log(data);
-	   if (data.hosts[0].target_login == null) {
-			streamOffline();
-	   }
-	   else {
-       document.getElementById('player').src = "https://player.twitch.tv/?channel=" + data.hosts[0].target_login +"&autplay=false";
+    type: 'GET',
+    url: 'https://api.twitch.tv/kraken/streams/' + username,
+    headers: {
+    'Client-ID': 'f2cmg4s30fnzmq7zbcx8rcsfxdc1san'
+    },
+    success: function(data) {
+        console.log(data);
+        if (data.stream) {
+            online();
+        }
+        else {
+            $.ajax({
+                type: 'GET',
+                url: 'https://cors-anywhere.herokuapp.com/https://tmi.twitch.tv/hosts?include_logins=1&host=' + userId,
+                success: function(data) {
+                    console.log(data);
+                    if (data.hosts[0].target_login == null) {
+                    streamOffline();
+                }
+                else {
+                    var hostee = data.hosts[0].target_login;
+                    $("#player").attr("src", "https://player.twitch.tv/?channel=" + hostee +"&autoplay=false")
+                    $("#openApp").attr("href", "twitch://open?stream=" + hostee)
 
-		//get title, viewers, game, etc from hosted streamer
-		function getHostInfo() {
-		$.ajax({
-		 type: 'GET',
-		 url: 'https://api.twitch.tv/kraken/streams/' + data.hosts[0].target_login,
-		 headers: {
-		   'Client-ID': 'f2cmg4s30fnzmq7zbcx8rcsfxdc1san'
-		 },
-		 success: function(data) {
-		   console.log(data);
-		   if (data.stream) {
+                    //get title, viewers, game, etc from hosted streamer
+                    function getHostInfo() {
+                        $.ajax({
+                            type: 'GET',
+                            url: 'https://api.twitch.tv/kraken/streams/' + data.hosts[0].target_login,
+                            headers: {
+                                'Client-ID': 'f2cmg4s30fnzmq7zbcx8rcsfxdc1san'
+                            },
+                            success: function(data) {
+                                console.log(data);
+                                if (data.stream) {
+                                    $("#title").text("Panda is currently hosting " + data.stream.channel.display_name);
+                                    $("#streaminfo").text(data.stream.channel.status);
+                                    chatSrc = "https://www.twitch.tv/" + data.stream.channel.name + "/chat";
+                                }
+                                else streamOffline();
+                            }
+                        });
 
-		   document.getElementById('title').textContent = "Panda is currently hosting " + data.stream.channel.display_name;
-		   document.getElementById('streaminfo').textContent = data.stream.channel.status;
-		   chatSrc = "https://www.twitch.tv/" + data.stream.channel.name + "/chat";
-		   }
-		   else streamOffline();
-		 }
-		});
 
+                    }
+                    //auto update 10s interval
+                    getHostInfo();
+                    setInterval(getHostInfo,10000);
 
-		}
-		//auto update 10s interval
-		getHostInfo();
-		setInterval(getHostInfo,10000);
-
-	   }
-	 },
-	 error: function()
-	 {
-		 playerError();
-	 }
-	});
-   }
- },
- error: function() {
-	 playerError();
- }
+                }
+                },
+                error: function() {
+                    playerError();
+                }
+            });
+        }
+    },
+    error: function() {
+        playerError();
+    }
 });
--- js/streamFunctions.js
+++ js/streamFunctions.js
@@ -1,79 +1,69 @@
-/* streamFunctions.js
- * Copyright (C) mattunderscore.us - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- */
+/*
+	streamFunctions.js
+	Copyright (C) Matt Jones - All Rights Reserved
+*/
 var username = "pandafite";
 var userId = "53010272";
 var chatSrc;
 
 function online() {
-	document.getElementById('player').src = "https://player.twitch.tv/?channel=" + username +"&autplay=false";
-	function getInfo(){
-
+	$("#player").attr("src", "https://player.twitch.tv/?channel=" + username +"&autoplay=false");
+	function getInfo() {
 		$.ajax({
-		 type: 'GET',
-		 url: 'https://api.twitch.tv/kraken/streams/' + username,
-		 headers: {
-		   'Client-ID': 'f2cmg4s30fnzmq7zbcx8rcsfxdc1san'
-		 },
-		 success: function(data) {
-		   console.log(data);
-		   document.getElementById('title').textContent = "LIVE: " + data.stream.channel.status;
-		   document.getElementById('streaminfo').textContent = "Playing " + data.stream.game + " for " + data.stream.viewers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " viewers and " + data.stream.channel.followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " followers";
-		 }
+			type: 'GET',
+			url: 'https://api.twitch.tv/kraken/streams/' + username,
+			headers: {
+				'Client-ID': 'f2cmg4s30fnzmq7zbcx8rcsfxdc1san'
+			},
+			success: function(data) {
+				console.log(data);
+				$("#title").text("LIVE: " + data.stream.channel.status);
+				$("#streaminfo").text("Playing " + data.stream.game + " for " + data.stream.viewers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+				 + " viewers and " + data.stream.channel.followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " followers");
+			}
 		});
 	} // end getInfo
 
 	getInfo();
 	setInterval(getInfo,10000);
-	}
+}
 
 function ytDisplay() {
 	$.ajax({
-		 type: 'GET',
-		 url: 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UU9_eMg0RM31CFHIoFOd4Log&maxResults=1&key=AIzaSyDRGoNzXk7wVpE2lCXG9SS7wPMZhmFSEhI',
-		 success: function(data) {
+		type: 'GET',
+		url: 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UU9_eMg0RM31CFHIoFOd4Log&maxResults=1&key=AIzaSyDRGoNzXk7wVpE2lCXG9SS7wPMZhmFSEhI',
+		success: function(data) {
 			console.log(data);
-			document.getElementById('yt-title').textContent = "Recent Video: " + data.items[0].snippet.title;
-			document.getElementById('yt-player').src = "https://www.youtube.com/embed/" + data.items[0].snippet.resourceId.videoId;
-
-
-		 }, //end success
-		 error: function () {
-		}
-		});	// end ajax
-
+			$("#yt-title").text("Recent Video: " + data.items[0].snippet.title);
+			$("#yt-player").attr("src", "https://www.youtube.com/embed/" + data.items[0].snippet.resourceId.videoId);
+		} //end success
+	});	// end ajax
 }
 
-function streamOffline()
-{
+function streamOffline() {
 	$.ajax({
-	 type: 'GET',
-	 url: 'https://api.twitch.tv/kraken/channels/' + username + '/videos?broadcasts=true',
-	 headers: {
-	   'Client-ID': 'f2cmg4s30fnzmq7zbcx8rcsfxdc1san'
-	 },
-	 success: function(data) {
-	   console.log(data);
-
-	   	if (data._total == 0) {
-			document.getElementById('title').textContent = "No streams found";
-			document.getElementById('player').src = "https://static-cdn.jtvnw.net/ttv-static/404_preview-800x450.jpg";
-		}
-
-		else {
-			document.getElementById('title').textContent = "Most recent broadcast:";
-			document.getElementById('streaminfo').textContent = " " + data.videos[0].title;
-			document.getElementById('player').src = "https://player.twitch.tv/?video=" + data.videos[0]._id + "&autplay=false";
-		}
-
-	 }
-	});
+		type: 'GET',
+		url: 'https://api.twitch.tv/kraken/channels/' + username + '/videos?broadcasts=true',
+		headers: {
+			'Client-ID': 'f2cmg4s30fnzmq7zbcx8rcsfxdc1san'
+		},
+		success: function(data) {
+			console.log(data);
+			if (data._total == 0) {
+				$("#title").text("No streams found");
+				$("#player").attr("src", "https://static-cdn.jtvnw.net/ttv-static/404_preview-800x450.jpg");
+			}
+			else {
+				$("#title").text("Most recent broadcast:");
+				$("#streaminfo").text(" " + data.videos[0].title);
+				$("#player").attr("src", "https://player.twitch.tv/?video=" + data.videos[0]._id + "&autoplay=false");
+			}
+		} // end success
+	}); // end ajax
 	chatSrc = "https://www.twitch.tv/" + username + "/chat";
 }
 
 function playerError() {
-	document.getElementById('player').src = "https://static-cdn.jtvnw.net/ttv-static/404_preview-800x450.jpg";
-	document.getElementById('title').textContent = "Error loading video";
+	$("#player").attr("src", "https://static-cdn.jtvnw.net/ttv-static/404_preview-800x450.jpg");
+	$("#title").text("Error loading video");
 }
